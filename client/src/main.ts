276a1d80acae75df4090f1b248d07e71c8ffa23a
Stephen Schier <stephenjschier@gmail.com>
2025-06-09 21:09:12 -0500

    Configure API environment and Render support

--- client/src/main.ts
+++ client/src/main.ts
@@ -35,7 +35,7 @@ API Calls
 */
 
 const fetchWeather = async (cityName: string) => {
-  const response = await fetch('/api/weather', {
+  const response = await fetch('import.meta.env.VITE_API_URL!', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
